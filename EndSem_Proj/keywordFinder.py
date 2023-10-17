689f21f0861a372b9758e2f7224dae4630629015
shlok-007 <shlok845@gmail.com>
2023-10-17 19:42:13 +0530

    Initialized EndSem project

---
+++ EndSem_Proj/keywordFinder.py
@@ -0,0 +1,19 @@
+import os
+
+def search_files_for_string(folder_path, target_string):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    if target_string in content:
+                        print(f"String found in file: {file_path}")
+            except (UnicodeDecodeError, PermissionError):
+                # Skip files that can't be read (e.g., binary files) or have permission issues
+                pass
+
+# Example usage: searching for the string "example" in files in the folder "path/to/folder"
+folder_path = "gpgpu-sim_distribution-4.0.1/src"
+target_string = "m_supervised_warps"
+search_files_for_string(os.path.expanduser(folder_path), target_string)
