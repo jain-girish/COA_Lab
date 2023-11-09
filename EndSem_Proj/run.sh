1a107f783d7fc1d2079ff6fdd0c1b8bb3312589e
shlok-007 <shlok845@gmail.com>
2023-11-09 23:15:57 +0530

    Added gpu config files

--- EndSem_Proj/run.sh
+++ EndSem_Proj/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd ~/Desktop/COA_Lab/EndSem_Proj/testing/gpu-rodinia/cuda/hotspot
-source ../gpgpu-sim_distribution-4.0.1/setup_environment
-nvcc -lcudart hotspot.cu
-./a.out > out.txt
-python3 clean.py
-
-# cd ~/Desktop/COA_Lab/EndSem_Proj/testing
+# cd ~/Desktop/COA_Lab/EndSem_Proj/testing/gpu-rodinia/cuda/hotspot
 # source ../gpgpu-sim_distribution-4.0.1/setup_environment
-# nvcc -lcudart mmul.cu
+# nvcc -lcudart hotspot.cu
 # ./a.out > out.txt
-# python3 clean.py
+# python3 ../../../clean.py
+
+cd ~/Desktop/COA_Lab/EndSem_Proj/testing
+source ../gpgpu-sim_distribution-4.0.1/setup_environment
+nvcc -lcudart mmul.cu
+./a.out > out_w_ka.txt
+python3 clean.py
